9839a385188d55e92a9b6b3e073d35b7f031872d
atena <atenab627@gmail.com>
2022-04-26 03:38:56 -0400

    dsds

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Session } from './auth/session.schema'
-
-/**@type {Session}*/
-export * from './auth/index'
-export * from './panel/indx'
-export * from './product/index'
-export * from './scene/index'
-export * from './user/index'
